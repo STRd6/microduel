5735ee8f796a9551e203c6ab3d8e74e2c0ed4ea2
Daniel X Moore <yahivin@gmail.com>
2010-03-12 19:59:36 -0800

    Using test passwords for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
+  acts_as_authentic do |c|
+    c.require_password_confirmation = false
+  end
 
-  before_validation_on_create :reset_password
+  #before_validation_on_create :reset_password
 
-  attr_accessible :email, :display_name
+  attr_accessible :email, :display_name, :password
 
   def display_name
     super || "Guest#{id}"
